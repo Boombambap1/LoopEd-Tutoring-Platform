82fb16687915482d179b393d4cffb0abc892385a
Boombambap1 <stevenw000098@gmail.com>
2025-09-27 16:27:52 -0700

    Add admin user creation to deployment

---
+++ accounts/management/commands/ensure_admin.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Create admin user if it doesn\'t exist'
+    
+    def handle(self, *args, **options):
+        User = get_user_model()
+        username = 'admin'
+        email = 'admin@looped.com'
+        password = 'admin123'  # Change this!
+        
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username, email, password)
+            self.stdout.write(
+                self.style.SUCCESS(f'Admin user "{username}" created successfully!')
+            )
+        else:
+            self.stdout.write(
+                self.style.WARNING(f'Admin user "{username}" already exists')
+            )
